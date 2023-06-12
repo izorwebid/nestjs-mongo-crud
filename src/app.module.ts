63e3f20b218a8c38c82434da9ed2fbf712d182e5
rozi <izorvmp@gmail.com>
2023-06-13 00:17:44 +0700

    update database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StudentSchema } from './schema/student.schema';
 import { StudentService } from './student/student.service';
 import { StudentController } from './student/student.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://vampire:dracula86@cluster0.zztdkju.mongodb.net',
-      {
-        dbName: 'crud',
-      },
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: 'crud',
+    }),
     MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
   ],
   controllers: [AppController, StudentController],
